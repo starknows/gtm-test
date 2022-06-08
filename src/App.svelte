c9717bc05eb49631e423294cc2f21c105d1e415d
Cheng-An Ko <gary@kingone-design.com>
2022-06-08 17:31:12 +0800

    init

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+    let listIsOpen = true
+    function openList() {
+        listIsOpen = !listIsOpen
+    }
+    function productClick(num) {
+        alert(num)
+    }
+</script>
+
+<main>
+    <h1>GTM測試用網頁</h1>
+    <button id="listSwitch" on:click={openList}>Switch List</button>
+    <div class="{listIsOpen ? 'open' : 'close'} product-list">
+        <div class="product" id="item1" on:click={() => productClick(1)}>1</div>
+        <div class="product" id="item2" on:click={() => productClick(2)}>2</div>
+        <div class="product" id="item3" on:click={() => productClick(3)}>3</div>
+        <div class="product" id="item4" on:click={() => productClick(4)}>4</div>
+    </div>
+</main>
+
+<style>
+    main {
+        min-height: 90vh;
+    }
+    .product-list {
+        height: 600px;
+    }
+    .product-list.close {
+        height: 0;
+        overflow: hidden;
+    }
+    .product {
+        height: 100px;
+        height: 100px;
+        background: pink;
+        margin: 50px;
+    }
+</style>

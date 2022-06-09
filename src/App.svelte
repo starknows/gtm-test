5b44f7a46957315181938230a897241a141c6df9
Cheng-An Ko <gary@kingone-design.com>
2022-06-09 09:35:50 +0800

    new btn

--- src/App.svelte
+++ src/App.svelte
@@ -6,11 +6,19 @@
     function productClick(num) {
         alert(num)
     }
+    function customEvent() {
+        let dataLayer = window.dataLayer || []
+        dataLayer.push({
+            event: 'custom_click',
+            check_text: 'success',
+        })
+    }
 </script>
 
 <main>
     <h1>GTM測試用網頁</h1>
     <button id="listSwitch" on:click={openList}>Switch List</button>
+    <button on:click={customEvent}>Custom Event</button>
     <div class="{listIsOpen ? 'open' : 'close'} product-list">
         <div class="product" id="item1" on:click={() => productClick(1)}>1</div>
         <div class="product" id="item2" on:click={() => productClick(2)}>2</div>
